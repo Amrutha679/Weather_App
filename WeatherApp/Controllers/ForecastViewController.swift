945c1eff660082d708c87edc230ec62225d95b46
Amrutha679 <amruthaedu665@gmail.com>
2020-11-18 17:27:57 +0530

    getting data from API

--- WeatherApp/Controllers/ForecastViewController.swift
+++ WeatherApp/Controllers/ForecastViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 
 class ForecastViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
 
         // Do any additional setup after loading the view.
     }
